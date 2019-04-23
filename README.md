5bdedf73d7b4de340deca63f61a269b9e0abdc41
Brush <25190163+Brush-JIM@users.noreply.github.com>
2019-04-23 23:25:02 +0800

    Rebuild

--- README.md
+++ README.md
@@ -1,45 +1,22 @@
 # 优学院-YouXueYuan-JavaScript
-### 脚本自动修改视频调速选项，若需要，请手动选择倍速（每个视频都要手动）。
-### 慎用调速！！！
-### 慎用调速！！！
-### 慎用调速！！！
+
 * 可用来看优学院视频而不用手动点击
 * 答题请手动，项目没打算增加答题功能  
   
 项目地址  
-GitHub:[https://github.com/Brush-JIM/YouXueYuan-JavaScript](https://github.com/Brush-JIM/YouXueYuan-JavaScript)  
-Bitbucket:[https://bitbucket.org/Brush-JIM/youxueyuan-javascript/](https://bitbucket.org/Brush-JIM/youxueyuan-javascript/)  
-Greasy Fork:[https://greasyfork.org/zh-CN/scripts/382033-优学院-youxueyuan-看视频](https://greasyfork.org/zh-CN/scripts/382033-优学院-youxueyuan-看视频)  
+[GitHub](https://github.com/Brush-JIM/YouXueYuan-JavaScript)、[Bitbucket](https://bitbucket.org/Brush-JIM/youxueyuan-javascript/)、[Greasy Fork](https://greasyfork.org/zh-CN/scripts/382033-优学院-youxueyuan-看视频)  
   
 Python程序版请到隔壁  
-GitHub:[https://github.com/Brush-JIM/YouXueYuan-Python](https://github.com/Brush-JIM/YouXueYuan-Python)  
-Bitbucket:[https://bitbucket.org/Brush-JIM/youxueyuan-python/](https://bitbucket.org/Brush-JIM/youxueyuan-python/)  
+[GitHub](https://github.com/Brush-JIM/YouXueYuan-Python)、[Bitbucket](https://bitbucket.org/Brush-JIM/youxueyuan-python/)  
   
-# 食用方法  
-* 安装Tampermonkey（推荐，脚本是在Tampermonkey下编写，使用正常）或Greasemonkey或Violentmonkey，安装方法在后面的“注意”标题
-* 访问  
-    GitHub:[https://raw.githubusercontent.com/Brush-JIM/YouXueYuan-JavaScript/master/优学院看视频.user.js](https://raw.githubusercontent.com/Brush-JIM/YouXueYuan-JavaScript/master/优学院看视频.user.js)  
-    or  
-    Bitbucket:[https://bitbucket.org/Brush-JIM/youxueyuan-javascript/src/master/优学院看视频.user.js](https://bitbucket.org/Brush-JIM/youxueyuan-javascript/src/master/优学院看视频.user.js)  
-  安装脚本“优学院看视频.user.js”到Tampermonkey/Violentmonkey/Greasemonkey中  
-* 进入课程，右上角的“咨询客服”会修改成其他按钮，点击改后的按钮即可  
-
-# 注意
-* 脚本依赖油猴实现  
-  本人使用Tampermonkey  
-  Google chrome请安装[Tampermonkey](https://tampermonkey.net/)或[Violentmonkey](https://violentmonkey.github.io/)  
-  Firefox请安装[Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)或[Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)或[Violentmonkey](https://addons.mozilla.org/zh-CN/firefox/addon/violentmonkey/)  
-  其他浏览器请看[https://greasyfork.org/zh-CN](https://greasyfork.org/zh-CN)  
-* 理论上无论是Tampermonkey还是Violentmonkey抑或是Greasemonkey，脚本都是互通的，都可以用，只是理论上！请自行测试  
+## 食用方法  
+* 安装脚本管理器，方法请看[Greasy Fork](https://greasyfork.org/zh-CN)
+* 访问  [Greasy Fork](https://greasyfork.org/zh-CN/scripts/382033-优学院-YouXueYuan-看视频)
+  安装脚本“优学院看视频.user.js”到脚本管理器中  
+* 进入课程，右下角有按钮  
 
 # 日志
 CHANGELOG.md [GitHub](https://github.com/Brush-JIM/YouXueYuan-JavaScript/blob/master/CHANGELOG.md) or [Bitbucket](https://bitbucket.org/Brush-JIM/youxueyuan-javascript/src/master/CHANGELOG.md?fileviewer=file-view-default)  
-  
-# 最后  
-我读的专业不是编程方向的，纯靠兴趣爱好  
-且js没学过，技术纯属渣渣  
-所以出现问题请不要大喷  
-按钮为什么在那个位置？——我没学过网页设计，对这个一窍不通，所以怎么简单怎么来😂  
   
 ---
 代码开源、免费  
--- 优学院看视频.user.js
+++ 优学院看视频.user.js
@@ -1,154 +1,211 @@
 // ==UserScript==
 // @name         优学院-YouXueYuan-看视频
 // @namespace    https://github.com/Brush-JIM/YouXueYuan-JavaScript
-// @version      0.4
+// @version      1.0
 // @description  js没学过，技术纯属渣渣。可用来看优学院视频而不用手动点击。
 // @author       Brush-JIM
 // @match        https://ua.ulearning.cn/learnCourse/learnCourse.html?courseId=*&chapterId=*
-// @grant        none
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @grant        unsafeWindow
-// @run-at       document-idle
+// @run-at       document-start
 // @icon         https://www.ulearning.cn/ulearning/favicon.ico
 // ==/UserScript==
+
 (function() {
     'use strict';
-    //删除开小差提示框
-    document.querySelector("[data-bind=\"if: $root.modalType() == 'suspend'\"]").parentNode.removeChild(document.querySelector("[data-bind=\"if: $root.modalType() == 'suspend'\"]"));
-    var elements = document.getElementsByClassName('operating-area')[0];
-    //删除客服按钮
-    elements.removeChild(document.getElementsByClassName('custom-service')[0]);
     //删除统计元素
     document.getElementById("statModal").parentNode.removeChild(document.getElementById("statModal"));
     //删除指导元素
     document.getElementsByClassName("user-guide")[0].parentNode.removeChild(document.getElementsByClassName("user-guide")[0]);
-    //在原客服按钮增加上车按钮
-    var div = document.createElement("div");
-    div.setAttribute("class", "custom-service");
-    div.setAttribute("style", "z-index:999999");
-    div.innerHTML='<button class="btn-hollow" onclick="start_watch();"><span>上上上车~~~</span></button>';
-    elements.appendChild(div);
+    //侧边栏
+    var cbl = document.createElement("div");
+    cbl.setAttribute("style", "width: 60px;height: 74px;overflow: hidden;position: fixed;right: 10px;bottom: 10%;padding: 4px;background-color: #06a9f4;z-index: 9999;/* border-radius: 8px 0px 0px 8px; *//* box-shadow: rgba(0, 85, 255, 0.098) 0px 0px 20px 0px; */border: 1px solid rgb(233, 234, 236);");
+    //cbl.setAttribute('style',"display:table-cell;*display:inline-block;*float:left;width:60px;height:70px;vertical-align:middle;border:1px solid #e3e3e9;margin:0;background-color:#fff;");
+    cbl.setAttribute("id","set-auto");
+    cbl.innerHTML='<div style="cursor: pointer; text-align: center; padding: 0px;"><span id="set-auto" style="font-size: 12px;line-height: 16px;color: rgb(255, 255, 255);">设置<br />&<br />开关</span></div>';
+    document.querySelector('body').appendChild(cbl);
+    var xfk = document.createElement('div');
+    xfk.setAttribute("style", "position: fixed;height: 300px;bottom: 10%;z-index: 9999;right: 70px; display: none;");
+    xfk.setAttribute("id", "set-mune-hide");
+    xfk.innerHTML = '<div style="display: block;overflow: hidden;height: 300px;width: 300px;/* border-radius: 8px; *//* box-shadow: rgba(106, 115, 133, 0.22) 0px 6px 12px 0px; */border: 1px solid rgb(233, 234, 236);background-color: rgb(255, 255, 255);"><div style="display: block; border-bottom: 1px solid rgb(230, 230, 230); height: 35px; line-height: 35px; margin: 0px; padding: 0px; overflow: hidden;"><span style="float: left;display: inline;padding-left: 8px;font: 700 14px/35px SimSun;">设置 & 开关</span></div><div style="display: block; position: absolute; top: 36px; width: 100%; height: calc(100% - 36px);"><div style="height: 100%; overflow: auto; padding: 0px 12px; margin: 0px;"><div><label style="display: inline;" title="调速">现在倍速：<input id="speed" type="text"></label></div><div><label style="display: inline;" title="静音"><input id="video_muted" type="checkbox">静音</label></div><div><label style="display: inline;" title="开始/停止"><button id="startstop">开始学习</button></label></div></div></div></div>';
+    document.querySelector('body').appendChild(xfk);
+    var speed;
+    var muted;
+    if (unsafeWindow.localStorage.getItem('speed') == null)
+    {
+        speed = '1';
+    }
+    else
+    {
+        speed = unsafeWindow.localStorage.getItem('speed');
+    }
+    if (unsafeWindow.localStorage.getItem('muted') == null)
+    {
+        muted = false;
+    }
+    else
+    {
+        if (unsafeWindow.localStorage.getItem('muted') == 'true')
+        {
+            muted = true;
+        }
+        else
+        {
+            muted = false
+        }
+    }
     for (var i = 0;i <= 6;i++) {
         setTimeout(function() {
-            //如果有倒计时，则删除
-            try
+            if (unsafeWindow.koLearnCourseViewModel == undefined)
             {
-                document.getElementsByClassName("count-down")[0].parentNode.removeChild(document.getElementsByClassName("count-down")[0]);
+                console.log('函数查找失败');
             }
-            catch (error) {
-                console.log(error);
+            else
+            {
+                console.log('函数存在')
             }
-        }, 3000);
-    };
-    //定义开始函数
-    window.start_watch = function() {
-        //删除开始按钮并增加停止按钮
-        elements.removeChild(document.getElementsByClassName('custom-service')[0]);
-        var div = document.createElement("div");
-        div.setAttribute("class", "custom-service");
-        div.setAttribute("style", "z-index:999999");
-        div.innerHTML='<button class="btn-hollow" onclick="stop_watch();"><span>刹刹刹车~~~</span></button>';
-        elements.appendChild(div);
-        window.myVar = setInterval(function(){
-            //window.tiankong();
-            if (document.getElementsByClassName("file-media")[0] != null) {
-                if (document.querySelector("[aria-label='Pause']") != null) {
-                    if (document.querySelector("[data-bind='text: $root.i18nMessageText().finished']") == null) {
-                        console.log('还未播放完');
-                        return ;
-                    }
-                    else {
-                        console.log('显示已看完，但是还未播完');
-                    }
-                }
-                console.log('视频');
-                if (document.querySelector("[data-bind='text: $root.i18nMessageText().finished']") == null) {
-                    console.log('开始播放视频');
-                    document.querySelector("[aria-label='Play']").click();
-                    //修改倍速，1.5倍速变15倍速，1.25倍速变10倍速，0.75变5倍速
-                    //请确定网速能跟得上
-                    document.querySelector("input[value='1.50']").value = 15;
-                    document.querySelector("input[value='1.25']").value = 10;
-                    document.querySelector("input[value='0.75']").value = 5;
-                    for (var x = 0;x < document.querySelectorAll("label[class='mejs__speed-selector-label']").length;x++)
-                    {
-                        if (x == 0)
-                        {
-                            document.querySelectorAll("label[class='mejs__speed-selector-label']")[x].innerHTML = '15.00x';
-                        }
-                        else if (x == 1)
-                        {
-                            document.querySelectorAll("label[class='mejs__speed-selector-label']")[x].innerHTML = '10.00x';
-                        }
-                        else
-                        {
-                            document.querySelectorAll("label[class='mejs__speed-selector-label']")[x].innerHTML = '5.00x';
-                        }
-                    }
-                    return ;
-                }
-                else {
-                    console.log('视频播放完毕');
-                }
+        }, 6000);
+    }
+    document.querySelector('input[id="speed"]').value = speed;
+    document.querySelector('input[id="video_muted"]').checked = muted;
+    $("#set-auto").click(
+        function()
+        {
+            if (document.querySelector('body').querySelector('span[id="set-auto"]').innerText == '设置\n&\n开关')
+            {
+                document.querySelector('div[id="set-mune-hide"]').setAttribute('style','position: fixed;height: 300px;bottom: 10%;z-index: 9999;right: 70px;');
+                document.querySelector('div[id="set-mune-hide"]').setAttribute('id','set-mune-unhide');
+                document.querySelector('body').querySelector('span[id="set-auto"]').innerHTML = '保存<br />or<br />隐藏';
             }
-            console.log('下一页');
-            window.koLearnCourseViewModel.goNextPage();
-            for (var i = 0; i < document.querySelectorAll("[data-bind='text: $root.nextPageName()']").length; ++i) {
-                console.log(document.querySelectorAll("[data-bind='text: $root.nextPageName()']")[i].innerHTML);
-                if (document.querySelectorAll("[data-bind='text: $root.nextPageName()']")[i].innerHTML == "没有了") {
-                    setTimeout(function(){
-                        try
-                        {
-                            window.koLearnCourseViewModel.goBack();
-                        }
-                        catch (error)
-                        {
-                            console.log(error);
-                        }
-                    },3000);
-                    clearInterval(window.myVar);
-                    return (true);
-                };
-            };
-            if (document.getElementsByClassName('modal-backdrop fade in')[0] != null) {
-                console.log('提示框');
-                if (document.querySelector("[data-bind='text: $root.i18nMsgText().gotIt']") != null) {
-                    console.log('视频提示');
-                    document.querySelector("[data-bind='text: $root.i18nMsgText().gotIt']").click();
-                }
-                else if (document.querySelector("[data-bind='text: $root.i18nMsgText().confirmLeave']") != null) {
-                    console.log('未完成确认');
-                    document.querySelector("[data-bind='text: $root.i18nMsgText().confirmLeave']").click();
-                }
-                else {
-                    alert('未知情况，暂无应对方案，请将本页面截屏，返回给作者。');
-                    return (false);
-                }
-            };
-            for (i = 0;i <= 6;i++) {
-                setTimeout(function() {
-                    //如果有倒计时，则删除
-                    try
-                    {
-                        document.getElementsByClassName("count-down")[0].parentNode.removeChild(document.getElementsByClassName("count-down")[0]);
-                    }
-                    catch (error) {
-                        console.log(error);
-                    }
-                }, 3000);
-            };
+            else
+            {
+                document.querySelector('div[id="set-mune-unhide"]').setAttribute('style','position: fixed;height: 300px;bottom: 10%;z-index: 9999;right: 70px; display: none;');
+                document.querySelector('div[id="set-mune-unhide"]').setAttribute('id','set-mune-hide');
+                document.querySelector('body').querySelector('span[id="set-auto"]').innerHTML = '设置<br />&<br />开关'
+                unsafeWindow.localStorage.setItem('speed',document.querySelector("input[id='speed']").value);
+                unsafeWindow.localStorage.setItem('muted',document.querySelector("input[id='video_muted']").checked);
+            }
         }
-                                   ,3000);
-    };
-    //定义停止函数
-    window.stop_watch = function() {
-        clearInterval(window.myVar);
-        //删除停止按钮并增加开始按钮
-        elements.removeChild(document.getElementsByClassName('custom-service')[0]);
-        var div = document.createElement("div");
-        div.setAttribute("class", "custom-service");
-        div.setAttribute("style", "z-index:999999");
-        div.innerHTML='<button class="btn-hollow" onclick="start_watch();"><span>上上上车~~~</span></button>';
-        elements.appendChild(div);
-        console.log('刹车成功');
-    };
+    );
+    $("#startstop").click(
+        function()
+        {
+            unsafeWindow.localStorage.setItem('speed',document.querySelector("input[id='speed']").value);
+            unsafeWindow.localStorage.setItem('muted',document.querySelector("input[id='video_muted']").checked);
+            if (document.querySelector('button[id="startstop"]').innerHTML == '开始')
+            {
+                unsafeWindow.watch_class = setInterval(function(){
+                    if (document.getElementsByClassName('modal-backdrop fade in')[0] != null) {
+                        console.log('提示框');
+                        if (document.querySelector("[data-bind='text: $root.i18nMsgText().gotIt']") != null) {
+                            console.log('视频提示');
+                            document.querySelector("[data-bind='text: $root.i18nMsgText().gotIt']").click();
+                        }
+                        else if (document.querySelector("[data-bind='text: $root.i18nMsgText().confirmLeave']") != null) {
+                            console.log('未完成确认');
+                            document.querySelector("[data-bind='text: $root.i18nMsgText().confirmLeave']").click();
+                        }
+                        else if (document.querySelector("[data-bind='text: $root.i18nMsgText().continueStudy']") != null)
+                        {
+                            console.log('走神提示');
+                            document.querySelector("[data-bind='text: $root.i18nMsgText().continueStudy']").click();
+                        }
+                        else {
+                            alert('未知情况，暂无应对方案，请将本页面截屏，返回给作者。');
+                            clearInterval(unsafeWindow.watch_class);
+                            return (false);
+                        }
+                    };
+                    var data = new Array();
+                    for (var i = 0;i < document.querySelectorAll('video').length;i++)
+                    {
+                        data[i] = new Array();
+                        data[i]["video"] = document.querySelectorAll('video')[i];
+                    }
+                    var counts = 0;
+                    for (var count = 0;count < document.querySelectorAll("div[class='video-info'] span").length;count++)
+                    {
+                        var data_bind = document.querySelectorAll("div[class='video-info'] span")[count].getAttribute('data-bind');
+                        if (data_bind == 'text: $root.i18nMessageText().finished')
+                        {
+                            data[counts]['s'] = true;
+                            counts++;
+                            console.log(true);
+                        }
+                        else if (data_bind == 'text: $root.i18nMessageText().viewed' || data_bind == 'text: $root.i18nMessageText().unviewed')
+                        {
+                            data[counts]['s'] = false;
+                            counts++;
+                            console.log(false);
+                        }
+                    }
+                    var all = true
+                    for (var j = 0;data.length > j;j++)
+                    {
+                        if (data[j]['s'] == false)
+                        {
+                            all = false;
+                            if (data[j]['video'].paused == true)
+                            {
+                                data[j]['video'].play();
+                                data[j]['video'].muted = muted;
+                                data[j]['video'].playbackRate = unsafeWindow.localStorage.getItem('speed');
+                                data[j]['video'].muted = unsafeWindow.localStorage.getItem('muted');
+                            }
+                            else
+                            {
+                                if (unsafeWindow.localStorage.getItem('muted') == 'true')
+                                {
+                                    if (data[j]['video'].muted != true)
+                                    {
+                                        data[j]['video'].muted = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (data[j]['video'].muted != false)
+                                    {
+                                        data[j]['video'].muted = false;
+                                    }
+                                }
+                                if (data[j]['video'].playbackRate != unsafeWindow.localStorage.getItem('speed'))
+                                {
+                                    data[j]['video'].playbackRate = unsafeWindow.localStorage.getItem('speed');
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (all == true)
+                    {
+                        console.log('下一页');
+                        unsafeWindow.koLearnCourseViewModel.goNextPage();
+                        for (var k = 0; k < document.querySelectorAll("[data-bind='text: $root.nextPageName()']").length; ++k) {
+                            console.log(document.querySelectorAll("[data-bind='text: $root.nextPageName()']")[k].innerHTML);
+                            if (document.querySelectorAll("[data-bind='text: $root.nextPageName()']")[k].innerHTML == "没有了") {
+                                setTimeout(function(){
+                                    try
+                                    {
+                                        unsafeWindow.koLearnCourseViewModel.goBack();
+                                    }
+                                    catch (error)
+                                    {
+                                        console.log(error);
+                                    }
+                                },1500);
+                                clearInterval(unsafeWindow.watch_class);
+                                return (true);
+                            };
+                        };
+                    }
+                },1500);
+                document.querySelector('button[id="startstop"]').innerHTML = '停止';
+            }
+            else
+            {
+                clearInterval(unsafeWindow.watch_class);
+                document.querySelector('button[id="startstop"]').innerHTML = '开始';
+            }
+        }
+    );
 })();
